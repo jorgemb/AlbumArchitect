3916b0057425899b248cfef4cc0bf66cd0dd1e42
Jorge L. Martinez <jorelmb@gmail.com>
2023-11-27 09:31:24 +0100

    Format check/fix.

--- source/album/face_classifier.cpp
+++ source/album/face_classifier.cpp
@@ -3,9 +3,10 @@
 //
 
 #include <filesystem>
-#include <config/config.h>
 
 #include "face_classifier.h"
+
+#include <config/config.h>
 
 namespace fs = std::filesystem;
 
@@ -14,12 +15,16 @@ namespace album_architect {
 auto FaceClassifier::get_opencv_face_detector()
     -> std::shared_ptr<cv::FaceDetectorYN> {
   return cv::FaceDetectorYN::create(
-      Config::get_cv_face_classifier_model().string(), "", cv::Size(320, 320), 0.8F);
+      Config::get_cv_face_classifier_model().string(),
+      "",
+      cv::Size(320, 320),
+      0.8F);
 }
 auto FaceClassifier::get_dlib_face_detector()
     -> std::shared_ptr<dlib_facenet_type> {
   auto detector = std::make_shared<dlib_facenet_type>();
-  dlib::deserialize(Config::get_dlib_face_classifier_model().string()) >> *detector;
+  dlib::deserialize(Config::get_dlib_face_classifier_model().string())
+      >> *detector;
 
   return detector;
 }
