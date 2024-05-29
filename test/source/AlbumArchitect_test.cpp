58770a5876eafb9041b2a0ec4b2aa249183d4cf9
Jorge L. Martinez <jorelmb@gmail.com>
2024-05-29 06:48:13 +0200

    Remove boilerplate lib
    This fixes #12

--- test/source/AlbumArchitect_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include "lib.hpp"
-
-TEST_CASE("Name is AlbumArchitect", "[library]") {
-  auto const lib = library {};
-  REQUIRE(lib.name == "AlbumArchitect");
-}
